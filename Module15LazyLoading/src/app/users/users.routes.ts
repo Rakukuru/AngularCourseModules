8ffec2cfbbf0615518787239fb3ce42c5e2063db
Rakukuru <raulsanchezmonera@hotmail.com>
2026-06-03 18:58:52 +0200

    Module 15 finished

--- Module15LazyLoading/src/app/users/users.routes.ts
+++ Module15LazyLoading/src/app/users/users.routes.ts
@@ -1,20 +1,47 @@
 import { Routes } from '@angular/router';
 
-import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+
+import { Task } from '../tasks/task/task.model';
+import { inject } from '@angular/core';
+import { TasksService } from '../tasks/tasks.service';
+import { ResolveFn } from '@angular/router';
+
+export const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
+
 
 export const routes: Routes = [
   {
     path: '',
+    providers: [TasksService],
     redirectTo: 'tasks',
     pathMatch: 'full',
   },
-  {
+  { //LAZY LOAD THE TASKS
     path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TasksComponent,
+    loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
     runGuardsAndResolvers: 'always',
     resolve: {
-      userTasks: resolveUserTasks,
+      userTasks: resolveUserTasks, // This function CANNOT BE loaded inside TasksComponent, otherwise there will be no lazy loading!
     },
   },
   {
